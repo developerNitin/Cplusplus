cdf1b51f1a1dae7ad8d5dcf23187dac7d2243f74
nitin <nitin.birdi@icloud.com>
2020-10-28 19:24:45 +0530

    commit#5:loop

---
+++ loop/dowhile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  do {
+    cin>>n;
+    cout<<n<<endl;
+  } while(n>0);
+
+  return 0;
+}  
---
+++ loop/while.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  while(n>0) {
+    cin>>n;
+    cout<<n<<endl;
+  }
+}
